631bc01a02643c737b3954566bb21b91e742abb2
akshatha-els <a.rk@elsevier.com>
2024-08-27 14:22:20 +0530

    journals

--- elseviershoppy/journals/admin.py
+++ elseviershoppy/journals/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import JournalItemDetails
+admin.site.register(JournalItemDetails)
--- elseviershoppy/journals/models.py
+++ elseviershoppy/journals/models.py
@@ -1,3 +1,15 @@
+# from django.db import models
+
+# # Create your models here.
 from django.db import models
 
 # Create your models here.
+class JournalItemDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=30,blank=False,default="science")
+    amount=models.IntegerField(blank=False)
+    category=models.CharField(max_length=30,blank=False,default="")
+    description = models.TextField(max_length=200,blank=False)
+    image=models.ImageField(upload_to='books/',blank=False)
+    def __str__(self):
+        return str(self.name)
