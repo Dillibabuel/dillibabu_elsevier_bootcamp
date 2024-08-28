b7a76de8867af0d55dacf43ce0004bad7d6711ff
NaazRMA <m.1@elsevier.com>
2024-08-28 14:38:13 +0530

    commit

--- elseviershoppy/logauth/urls.py
+++ elseviershoppy/logauth/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('register',views.register,name='register'),
     path('logout/', views.logout, name='logout'),
     path('forgotpassword',views.forgotpassword,name='forgotpassword'),
-    path('profile',views.profile,name='profile')
+    path('profile',views.profile,name='profile'),
+    
     
 ]
+
