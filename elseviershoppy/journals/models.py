449d9b6dbec30b517b25c6e0e8ad3c2f92cce862
akshatha-els <a.rk@elsevier.com>
2024-08-28 13:52:26 +0530

    jdetails
    journals-health

--- elseviershoppy/journals/models.py
+++ elseviershoppy/journals/models.py
@@ -6,10 +6,10 @@ from django.db import models
 # Create your models here.
 class JournalItemDetails(models.Model):
     id = models.AutoField(primary_key=True)
-    name=models.CharField(max_length=30,blank=False,default="science")
-    amount=models.IntegerField(blank=False)
+    name=models.CharField(max_length=30,blank=False,default="")
+    # amount=models.IntegerField(blank=False)
     category=models.CharField(max_length=30,blank=False,default="")
     description = models.TextField(max_length=200,blank=False)
-    image=models.ImageField(upload_to='books/',blank=False)
+    image=models.ImageField(upload_to='journals/',blank=False)
     def __str__(self):
         return str(self.name)
--- elseviershoppy/journals/urls.py
+++ elseviershoppy/journals/urls.py
@@ -2,4 +2,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
    path('',views.journals, name='journals'),
+   path('journals/',views.j_details, name='j_details'),
 ]
